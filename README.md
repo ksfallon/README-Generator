103fa0e9286d0eb9e1db7fcf4543607890acb5db
ksfallon <fallon.katelin@gmail.com>
2021-05-27 18:52:09 -0400

    updated readme so table of contents links worked, had to remove space before the #s and numbersgit add .

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
-# The Donminator
-  # Description:
-  - dominating
+# jet
+## Description:
+- this
+## Table of Contents:
+1. [Installation Section](#installation-instructions)
+2. [Usage Section](#usage-information)
+3. [Contribution Section](#contribution-guidelines)
+4. [Testx Section](#)
+5. [Questions Section](#questions)
+
+## Installation Instructions
+- how
+## Usage Information
+- that
+## Contribution Guidelines
+- nope
+## Test Instructions
+
+## 5. Questions:
+- Should you have any questions about the application that are not covered in my README or you run into any problems feel free to contact me through github at https://github.com/kate 
+- You can also reach me at my email address, ktos, with any additional questions.
+
 
 
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+// creating an If function but they are not longer if statements.
+// basically if license is for ex MIT pro
+  if (license === 'MIT') {
+    
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,26 +21,27 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  # Description:
-  - ${data.description}
-  # Table of Contents:
-    1. Installation Instructions.
-    2. Usage Information.
-    3. Contribution Guidelines.
-    4. Test Instructions.
-    5. Questions.
+## Description:
+- ${data.description}
+## Table of Contents:
+1. [Installation Section](#installation-instructions)
+2. [Usage Section](#usage-information)
+3. [Contribution Section](#contribution-guidelines)
+4. [Test Section](#)
+// any test commands
+5. [Questions Section](#questions)
 
-  # 1. Installation Instructions:
-  - ${data.installation}
-  # 2. Usage Information:
-  - ${data.usage}
-  # 3. Contribution Guidelines:
-  - ${data.contributions}
-  # 4. Test Instructions:
+## Installation Instructions
+- ${data.installation}
+## Usage Information
+- ${data.usage}
+## Contribution Guidelines
+- ${data.contributions}
+## Test Instructions
 
-  # 5. Questions:
-  - Should you have any questions about the application that are not covered in my README or you run into any problems feel free to contact me through github at https://github.com/${data.github} 
-  - You can also reach me at my email address, ${data.email}, with any additional questions.
+## 5. Questions:
+- Should you have any questions about the application that are not covered in my README or you run into any problems feel free to contact me through github at https://github.com/${data.github} 
+- You can also reach me at my email address, ${data.email}, with any additional questions.
 
 
 
